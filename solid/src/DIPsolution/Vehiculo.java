516f8899c3a0419ad52185831fc6cd41de97b869
ivinces <ivinces@espol.edu.ec>
2018-06-07 22:23:19 -0500

    DIP

---
+++ solid/src/DIPsolution/Vehiculo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DIPsolution;
+
+/**
+ *
+ * @author isabe
+ */
+public class Vehiculo {
+    public String asiento;
+    public String llanta;
+
+    public String getAsiento() {
+        return asiento;
+    }
+
+    public void setAsiento(String asiento) {
+        this.asiento = asiento;
+    }
+
+    public String getLlanta() {
+        return llanta;
+    }
+
+    public void setLlanta(String llanta) {
+        this.llanta = llanta;
+    }
+}
